bc626efcc32d6dcd9b4556930a9bb7591537c138
quarkusio <no-reply@quarkus.io>
2025-09-06 06:26:59 +0000

    Initial commit

---
+++ src/test/java/org/github/tru3d3v/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.github.tru3d3v;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
